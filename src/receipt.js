c11c7c4855afea25ed46453e47669e2e49b3fe53
Lee Kelly <leeowenkelly@gmail.com>
2023-06-19 14:24:50 +0100

    getSubtotal revised

--- src/receipt.js
+++ src/receipt.js
@@ -2,13 +2,14 @@ const Bagel = require("../src/bagel.js");
 const Basket = require("../src/basket.js");
 
 class Receipt {
-    constructor(obj = {}){
-    this.purchases = obj
-    this.date = new Date
-    this.total = 0
-    }
-    getReceipt(){
-        return `
+  constructor(obj = {}) {
+    this.purchases = obj;
+    this.date = new Date();
+    this.total = 0;
+  }
+
+  getReceipt() {
+    return `
     ~~~ Bob's Bagels ~~~    
 
        ${this.date.toDateString()}
@@ -16,35 +17,35 @@ class Receipt {
 ${this.getPurchaseList()}
 Total                 £${Number(this.total.toFixed(2))}
         Thank you
-      for your order!         `
-    }
-    getPurchaseList(){
-        this.total = 0
-        let purchaseLines = ""
-        for (let key in this.purchases){
-            let receiptLine = ""
-            receiptLine += Bagel.getTypeOfBagel(key)
-            ? Bagel.getTypeOfBagel(key)
-            : 'Coffee'
-            for (let i = 0;i<19;i++){
-                if (receiptLine.length < 19){
-                    receiptLine += " "
-                }
-            }
-            receiptLine += this.purchases[`${key}`]
-            for (let i=0;i<4;i++){
-                if (receiptLine.length < 23){
-                    receiptLine += " "
-                }
-            }
-            receiptLine += "£"
-            const subtotal = Basket.getSubtotal(this.purchases, key)
-            receiptLine += subtotal
-            this.total += subtotal
-            purchaseLines += `${receiptLine}\n`
-        }
-        return purchaseLines
-    }
+      for your order!         `;
+  }
 
+  getPurchaseList() {
+    this.total = 0;
+    let purchaseLines = "";
+    for (let key in this.purchases) {
+      let receiptLine = "";
+      receiptLine += Bagel.getTypeOfBagel(key)
+        ? Bagel.getTypeOfBagel(key)
+        : "Coffee";
+      for (let i = 0; i < 19; i++) {
+        if (receiptLine.length < 19) {
+          receiptLine += " ";
+        }
+      }
+      receiptLine += this.purchases[`${key}`];
+      for (let i = 0; i < 4; i++) {
+        if (receiptLine.length < 23) {
+          receiptLine += " ";
+        }
+      }
+      receiptLine += "£";
+      const subtotal = Basket.getSubtotal(this.purchases, key);
+      receiptLine += subtotal;
+      this.total += subtotal;
+      purchaseLines += `${receiptLine}\n`;
+    }
+    return purchaseLines;
+  }
 }
-module.exports = Receipt
+module.exports = Receipt;
