a3e8c0d61b68fc53caf3bea9e3904bda08bbcd70
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-12-19 15:22:52 +0000

    test: :test_tube: update new valid Bagel with quantity

--- spec/bagel.spec.js
+++ spec/bagel.spec.js
@@ -1,19 +1,21 @@
 const Bagel = require('../src/bagel.js')
 
 describe('Bagel:', () => {
-  it('new instance with valid sku', () => {
-    const bagel = new Bagel('BGLS')
+  describe('New -', () => {
+    it('new instance with valid sku', () => {
+      const bagel = new Bagel('BGLS', 3)
 
-    const expected = {
-      sku: 'BGLS',
-      price: '0.49',
-      name: 'Bagel',
-      variant: 'Sesame',
-      quantity: 1
-    }
+      const expected = {
+        sku: 'BGLS',
+        price: '0.49',
+        name: 'Bagel',
+        variant: 'Sesame',
+        quantity: 3
+      }
 
-    const result = bagel.details
+      const result = bagel.details
 
-    expect(result).toEqual(expected)
+      expect(result).toEqual(expected)
+    })
   })
 })
