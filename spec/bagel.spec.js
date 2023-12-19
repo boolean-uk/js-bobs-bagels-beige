9c771dcbcbf992eb2a8a3fea97192d5922998e2b
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-12-19 17:59:13 +0000

    test: :test_tube: add new bagel instance case when invalid quantity

--- spec/bagel.spec.js
+++ spec/bagel.spec.js
@@ -41,5 +41,11 @@ describe('Bagel:', () => {
 
       expect(bagel).toThrowError('sku not found')
     })
+
+    // it('instance with quantity not at least 1', () => {
+    //   const bagel = () => new Bagel('BGLS', 0.5)
+
+    //   expect(bagel).toThrowError('quantity must be at least 1')
+    // })
   })
 })
