60df8a33a9b70d558f76bb39a4a726f10294c2ed
Rebecca Noy <rebecca.noy@gmx.de>
2023-12-19 14:00:48 +0100

    add beforeEach to keep test dry

--- spec/basketItem.spec.js
+++ spec/basketItem.spec.js
@@ -2,17 +2,18 @@ const BasketItem = require("../src/basketItem.js");
 const { Bagel } = require("../src/bagel.js");
 
 describe("BasketItem", () => {
-  it("can be created if sku is valid", () => {
+  beforeEach(() => {
     const myBagel = new Bagel("BGLO", 1)
     const myBasketBagel = new BasketItem("BGLO")
+  })
+
+  it("can be created if sku is valid", () => {
     expect(myBasketBagel.sku).toEqual(myBagel.sku)
     expect(myBasketBagel.quantity).toEqual(1)
     expect(myBasketBagel.price).toEqual(myBagel.price)
   })
 
   it("returns proper subtotal", () => {
-    const myBagel = new Bagel("BGLO", 1)
-    const myBasketBagel = new BasketItem("BGLO")
     expect(myBasketBagel.getSubtotal()).toEqual(Number(myBagel.price))
   })
 })
