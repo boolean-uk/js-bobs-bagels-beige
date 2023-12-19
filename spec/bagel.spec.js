8653e5c38df8d155be5a1d5d6522b962ffaabca0
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-12-19 16:42:38 +0000

    test: :test_tube: add new bagel default quantity case

--- spec/bagel.spec.js
+++ spec/bagel.spec.js
@@ -1,8 +1,25 @@
 const Bagel = require('../src/bagel.js')
 
 describe('Bagel:', () => {
-  describe('New -', () => {
-    it('new instance with valid sku and custom quantity', () => {
+  describe('New', () => {
+    // it('instance with valid sku and default quantity', () => {
+    //   const bagel = new Bagel('BGSE')
+
+    //   const expected = {
+    //     sku: 'BGSE',
+    //     price: '2.99',
+    //     name: 'Bagel Sandwich',
+    //     variant: 'Everything',
+    //     fillings: ['Bacon', 'Egg', 'Cheese'],
+    //     quantity: 1
+    //   }
+
+    //   const result = bagel.details
+
+    //   expect(result).toEqual(expected)
+    // })
+
+    it('instance with valid sku and custom quantity', () => {
       const bagel = new Bagel('BGLS', 3)
 
       const expected = {
